e09078639ff975f2414cde154a73766e4b0cd825
SneaX-042 <83787007+SneaX-042@users.noreply.github.com>
2021-10-19 21:40:36 +0530

    Update ssos_avicii.mk

--- ssos_avicii.mk
+++ ssos_avicii.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/avicii/device.mk)
 
 # Inherit some common SSOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # SSOS Properties
 TARGET_GAPPS_ARCH := arm64
